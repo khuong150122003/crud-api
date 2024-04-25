29336d24c88eac37eaa0e45280e9899bf8087a63
Trần Nhật Khương <110782685+khuong150122003@users.noreply.github.com>
2024-04-25 21:51:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require("express");
 require("dotenv").config();
 const connectDB = require("./config/db");
 const userR = require("./routes/admin-routes");
-const productR = require("./routes/coordinator-router");
-const studentR = require("./routes/student-router");
-const orderR = require("./routes/manager-routes");
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -15,9 +12,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/admin", userR);
-app.use("/api/admin/classes", orderR);
-app.use("/api/coordinator", productR);
-app.use("/api/student", studentR);
 
 // Connect DataBase
 connectDB();
