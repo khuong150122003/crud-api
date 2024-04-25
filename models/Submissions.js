aed8520568843f2ce37417d76db757028b8075f7
Trần Nhật Khương <110782685+khuong150122003@users.noreply.github.com>
2024-04-25 22:00:27 +0700

    Update Submissions.js

--- models/Submissions.js
+++ models/Submissions.js
@@ -6,8 +6,11 @@ const submissionSchema = new Schema({
   class_name: { type: String, required: true },
   class_code: { type: String, required: true },
   course_name: { type: String, required: true },
+  status: { type: String },
   assignment_id: { type: String, required: true },
   submission_date: { type: Date, required: true },
+  title: { type: String },
+  description: { type: String },
   grade: { type: String },
   comment: { type: String },
   submission_file: { type: String }, // New field to store file path
