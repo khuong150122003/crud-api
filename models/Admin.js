e7108aebcace273073e023a983591a7b2ac73e4c
Trần Nhật Khương <110782685+khuong150122003@users.noreply.github.com>
2024-04-25 21:58:52 +0700

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  username: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true },
