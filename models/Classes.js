dd5a2c79768b24af347ccc7846cd7f8bdf83f874
Trần Nhật Khương <110782685+khuong150122003@users.noreply.github.com>
2024-04-25 21:59:08 +0700

    Update Classes.js

--- models/Classes.js
+++ models/Classes.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const classSchema = new Schema({
-  className: { type: String, required: true },
-  codeClass: { type: String, required: true },
+  name: { type: String, required: true },
+  code: { type: String, required: true },
   students: [{ type: Schema.Types.ObjectId, ref: "Student" }],
   courses: [{ type: Schema.Types.ObjectId, ref: "Courses" }],
+  // student_id: [{ type: Schema.Types.ObjectId, ref: "Student" }],
+  // coordinator_id: [{ type: Schema.Types.ObjectId, ref: "Coordinator" }],
 });
 
 module.exports = mongoose.model("Class", classSchema);
