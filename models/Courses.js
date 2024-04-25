21fbc2f62ad8f2569d604b0f6301c26696adc6ef
Trần Nhật Khương <110782685+khuong150122003@users.noreply.github.com>
2024-04-25 21:59:29 +0700

    Update Courses.js

--- models/Courses.js
+++ models/Courses.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const courseSchema = new Schema({
   name: { type: String, required: true },
   class_name: { type: String, required: true },
+  class_code: { type: String, required: true },
   start_date: { type: Date, required: true },
   end_date: { type: Date, required: true },
   documents: [{ type: Schema.Types.ObjectId, ref: "Document" }],
